e1f8ec7f1efbaf254dc730add308c17cdf67e3a2
Kelvin Chiu <kchiu1997@gmail.com>
2022-12-28 15:32:03 +0800

    Delete unused files

--- build/src/klogger/CMakeFiles/klogger.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libklogger.a"
-  "libklogger.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/klogger.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
